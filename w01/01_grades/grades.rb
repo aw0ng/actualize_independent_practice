0ea00a7e84ada65578481fd0ef8eb7b11941aeee
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-03-12 23:46:31 -0500

    w01 - 01_grades

--- w01/01_grades/grades.rb
+++ w01/01_grades/grades.rb
@@ -1,6 +1,20 @@
 def final_grade(array)
-
-  # This method currently has no code.
-  # Your job is to write the code to get the method to work!
-
+  if array.length == 0
+    p "I"
+  else mean_avg = array.sum / array.length
+       if mean_avg.round >= 90.0
+         p "A"
+       elsif (mean_avg.round >= 80.0) && (mean_avg.round <= 89.0)
+         p "B"
+       elsif (mean_avg.round >= 70.0) && (mean_avg.round <= 79.0)
+         p "C"
+       elsif (mean_avg.round >= 60.0) && (mean_avg.round <= 69.0)
+         p "D"
+       else mean_avg.round < 60.0
+            p "F"
+       end
+  end
 end
+
+grades = [95.0, 92.0, 99.0, 88.0, 96.0]
+final_grade(grades)
